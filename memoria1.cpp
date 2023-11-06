ccb9cfee212ef1bfe746e6b32be62f50fcf2d375
moctezumaximenix <148783500+moctezumaximenix@users.noreply.github.com>
2023-11-06 08:43:07 -0600

    Add files via upload

---
+++ memoria1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int var = 5;
+	
+	//declaración de la variable puntero
+	int* pointVar;
+	
+	//Almacena la dirección de var
+	pointVar = &var;
+	
+	//imprime el vaor de var
+	cout << "var = " << var << endl;
+	
+	//imprime la direccion de var
+	cout << "Direccion de la variable var (&var) = " << &var << endl << endl;
+	
+	//imprime el apuntador pointVar
+	cout << "Apuntador pointVar = " << pointVar << endl << endl;
+	
+	//cambia el valor de var 
+	cout << "cambia el valor de var a 7:" << endl << endl;
+	
+	//cambia el valor de var a 7
+	var = 7;
+	
+	//imprime el valor de var
+	cout << "var = " << var << endl << endl;
+	
+	//imprime el apuntador pointVar
+	cout << "*pointVar = " << *pointVar <<endl <<endl;
+	
+	cout << "Cambia el valor de *pointVar a 16:" <<endl;
+	
+	//cambia el valor de var a 16
+	*pointVar = 16;
+	
+	//imprime el valor de var
+	cout << "var = " << var << endl << endl;
+	
+	//imprime el apuntador pointVar
+	cout<< "Apuntador pointVar = " << pointVar <<endl;
+	//imprime el contenido del apuntador pointVar
+	cout << "*pointVar = " << *pointVar <<endl;
+	//imprime el valor de var
+	cout << "var = " << var << endl << endl;
+	
+	return 0;
+}
